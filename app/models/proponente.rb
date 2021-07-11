ed38904ea913936dd427d039634a9cc3a7860dbd
RicardoBucard <ricardobucard@gmail.com>
2021-07-11 07:53:10 -0300

    Adiciona validação dos campos do formulário, funções js de tradução de mensagem de erro e cálculo do desconto do inss

--- app/models/proponente.rb
+++ app/models/proponente.rb
@@ -1,2 +1,3 @@
 class Proponente < ApplicationRecord
+    validates :nome, :cpf, :data_nascimento, :logradouro, :numero, :bairro, :cidade, :estado, :cep, :telefone_pessoal, :telefone_referencia, :salario, :desconto_inss, presence: true
 end
