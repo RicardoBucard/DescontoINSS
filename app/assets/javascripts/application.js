b300dbc5e439cff48ad736e6f0ad255dd906eb21
RicardoBucard <ricardobucard@gmail.com>
2021-07-20 03:32:46 -0300

    refactor de código

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -103,3 +103,19 @@ toastr.options = {
     "showMethod": "fadeIn",
     "hideMethod": "fadeOut"
 }
+function maskValues(){
+    var maskBehavior = function (val) {
+      return val.replace(/\D/g, '').length === 11 ? '(00)00000-0000' : '(00)0000-00009';
+    },
+    options = {onKeyPress: function(val, e, field, options) {
+      field.mask(maskBehavior.apply({}, arguments), options, {removeMaskOnSubmit: true});
+      }
+    }
+    $('#telefone-pessoal').mask(maskBehavior, options, {removeMaskOnSubmit: true});
+    
+    $('#telefone-referencia').mask(maskBehavior, options, {removeMaskOnSubmit: true});
+    
+    $('#cep').mask('00000-000');
+    
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+}
