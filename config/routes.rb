3f69554db4aa6dfaa2bb9666324023c3c367c296
RicardoBucard <ricardobucard@gmail.com>
2021-08-23 10:42:39 -0300

    "Adiciona tela de relatorio_salarial no routes.rb e na tela de funcionarios"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :funcionarios
+  resources :funcionarios do 
+    collection do
+      get 'relatorio_salarial'
+    end
+  end
   resources :proponentes
   root to: "proponentes#index"
 
-  post 'calc/desconto/', to: "calc#desconto" 
+  post 'calc/desconto/', to: "calc#desconto"
 end
